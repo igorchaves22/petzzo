db96cb42c9790e9691f17ce377631c1efd03ae2c
Igor Gabiel Vieira Chaves <contato.igorgabriel@hotmail.com>
2025-04-07 16:39:56 -0300

    chore: Styles
    - Installation of Tailwindcss dependencies
    - Configuration of Tailwindcss files
    - New import (styles)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     server: {
         port: 3000
     },
@@ -13,6 +14,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
+            "~styles": path.resolve(__dirname, "./src/styles"),
             "~types": path.resolve(__dirname, "./src/types"),
             "~utils": path.resolve(__dirname, "./src/utils")
         }
